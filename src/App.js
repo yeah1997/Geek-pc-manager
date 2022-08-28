289c284f1e4d981b85211fe33a9669e81d41dd49
yeah1997 <lee7778889@gmail.com>
2022-08-28 13:37:51 +0900

    Init Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Login from './pages/Login'
+
 function App() {
-  return <div className="App">A</div>
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/home" component={Layout}></Route>
+          <Route path="/login" component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
